b8011f8f6f32f7a3684aa1583238f5afa8b8bce8
ArcaneSunku <t4hnner@gmail.com>
2024-12-25 06:07:47 -0600

    Fixed premake script

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "ImGui"
 	kind "StaticLib"
 	language "C++"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. OUTPUT_DIR .. "/%{prj.name}")
+	objdir ("bin-int/" .. OUTPUT_DIR .. "/%{prj.name}")
 
 	files
 	{
